cd6a05df512307bf9892e6be1de56d1392a4d78f
juhokortelainen <juho.kortelainen@hotmail.com>
2021-10-18 13:59:55 +0300

    Added Spring Security dependencies to pom.xml. Added Spring Security configuration class. Added two in.memory users USER and ADMIN. Added login functionality. Restricted delete and edit functionality to ADMIN role.

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,7 +47,7 @@ public class BookController {
 	}
 	
 	// RESTful servie to get book by id
-	@RequestMapping(value ="/books{id}", method = RequestMethod.GET)
+	@RequestMapping(value ="/books/{id}", method = RequestMethod.GET)
 	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {
 		return repository.findById(id);
 	}
@@ -67,12 +68,15 @@ public class BookController {
 	}
 
 	// poistetaan kirja id:n perusteella
+	@PreAuthorize(value = "hasRole('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long id, Model model) {
 		repository.deleteById(id);
 		return "redirect:../booklist";
 	}
 
+
+	@PreAuthorize(value = "hasRole('ADMIN')")
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String editBook(@PathVariable("id") Long id, Model model) {
 		Book book = repository.findById(id).orElse(null);
